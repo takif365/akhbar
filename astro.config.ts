8ec6a42d2987cba04f0b34ed9814cea07ab2e9e2
takif365 <99274515+takif365@users.noreply.github.com>
2026-08-21 17:19:02 -0400

    Change default locale to Arabic in configuration

--- astro.config.ts
+++ astro.config.ts
@@ -29,8 +29,8 @@ export default defineConfig({
     }),
   ],
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ["ar"],
+    defaultLocale: "ar",
     routing: {
       prefixDefaultLocale: false,
     },
